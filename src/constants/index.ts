cfc764c9d62c388fca4b35b011714d0ff9e8945a
bhavan <bhavanl@techprovint.com>
2023-07-28 22:54:06 +0530

    New components

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,29 @@
 export const OPEN_AI_ORGANIZATION_KEY = 'org-fUnU6QiypzI74bYqlRAfrlgR';
 export const OPEN_AI_API_KEY = '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';
+
+export const TEXT_CASES = [
+    { value: 'lowerCase', label: 'lowercase' },
+    { value: 'upperCase', label: 'UPPERCASE' },
+    { value: 'camelCase', label: 'camelCase' },
+    { value: 'capitalCase', label: 'Capital Case' },
+    { value: 'constantCase', label: 'CONSTANT_CASE' },
+    { value: 'dotCase', label: 'dot.case' },
+    { value: 'headerCase', label: 'Header-Case' },
+    { value: 'noCase', label: 'no case' },
+    { value: 'paramCase', label: 'param-case' },
+    { value: 'pascalCase', label: 'PascalCase' },
+    { value: 'pathCase', label: 'path/case' },
+    { value: 'sentenceCase', label: 'Sentence case' },
+    { value: 'snakeCase', label: 'snake_case' },
+];
+
+export const DATE_TYPES = [
+    { value: 0, label: 'toString' },
+    { value: 1, label: 'toISOString' },
+    { value: 2, label: 'toDateString' },
+    { value: 3, label: 'toTimeString' },
+    { value: 4, label: 'toUTCString' },
+    { value: 5, label: 'toLocaleTimeString' },
+    { value: 6, label: 'toLocaleDateString' },
+    { value: 7, label: 'toLocaleString' },
+];
--- src/pages/utility/index.tsx
+++ src/pages/utility/index.tsx
@@ -1,41 +1,177 @@
 import { Button, Flex, SimpleGrid } from '@mantine/core';
+import * as changeCase from 'change-case';
 import { useState } from 'react';
 import { TextFieldWithCopy } from '../../components';
+import { DATE_TYPES, TEXT_CASES } from '../../constants';
 
-const Utility = () => {
+const GetPublicIpComponent = () => {
     const [ip, setIp] = useState('');
-    const [timestamp, setTimestamp] = useState('');
-    const [loading, setLoading] = useState({ ip: false, timestamp: false });
+    const [loading, setLoading] = useState(false);
 
     const getPublicIp = async () => {
         setIp('');
-        setLoading(loading => ({ ...loading, ip: true }));
+        setLoading(true);
         const response = await fetch('https://api.ipify.org');
         const ip = await response.text();
         setIp(ip);
-        setLoading(loading => ({ ...loading, ip: false }));
-    };
-
-    const getCurrentTimestamp = () => {
-        setLoading(loading => ({ ...loading, timestamp: true }));
-        setTimestamp(String(Date.now()));
-        setLoading(loading => ({ ...loading, timestamp: false }));
+        setLoading(false);
     };
 
     return (
-        <SimpleGrid cols={2} spacing="sm" verticalSpacing="sm">
-            <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
-                <TextFieldWithCopy value={ip} setValue={setIp} />
-                <Button loading={loading.ip} onClick={getPublicIp}>
-                    Get Public IP
-                </Button>
-            </Flex>
-            <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
-                <TextFieldWithCopy value={timestamp} setValue={setTimestamp} />
-                <Button loading={loading.timestamp} onClick={getCurrentTimestamp}>
-                    Get Timestamp
-                </Button>
-            </Flex>
+        <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+            <TextFieldWithCopy value={ip} setValue={setIp} />
+            <Button loading={loading} onClick={getPublicIp}>
+                Get Public IP
+            </Button>
+        </Flex>
+    );
+};
+
+const GetTimestampComponent = () => {
+    const [timestamp, setTimestamp] = useState('');
+
+    const getCurrentTimestamp = () => {
+        setTimestamp(String(Date.now()));
+    };
+
+    return (
+        <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+            <TextFieldWithCopy value={timestamp} setValue={setTimestamp} />
+            <Button onClick={getCurrentTimestamp}>Get Timestamp</Button>
+        </Flex>
+    );
+};
+
+const ConvertTimestampComponent = () => {
+    const [ts, setTs] = useState('');
+    const [tsType, setTsType] = useState(0);
+    const [convertedTs, setConvertedTs] = useState('');
+
+    const get_time_value = (timestamp: any) => {
+        let date = new Date(+timestamp).toString();
+        return date === 'Invalid Date' ? `${timestamp}` : +timestamp;
+    };
+
+    const handleChangeTimestamp = () => {
+        var currentText = '';
+        const timeString = get_time_value(ts);
+        console.log(timeString);
+
+        switch (tsType) {
+            case DATE_TYPES[0].value:
+                currentText = new Date(timeString).toString();
+                break;
+            case DATE_TYPES[1].value:
+                currentText = new Date(timeString).toISOString();
+                break;
+            case DATE_TYPES[2].value:
+                currentText = new Date(timeString).toDateString();
+                break;
+            case DATE_TYPES[3].value:
+                currentText = new Date(timeString).toTimeString();
+                break;
+            case DATE_TYPES[4].value:
+                currentText = new Date(timeString).toUTCString();
+                break;
+            case DATE_TYPES[5].value:
+                currentText = new Date(timeString).toLocaleTimeString();
+                break;
+            case DATE_TYPES[6].value:
+                currentText = new Date(timeString).toLocaleDateString();
+                break;
+            case DATE_TYPES[7].value:
+                currentText = new Date(timeString).toLocaleString();
+                break;
+            default:
+                currentText = new Date(timeString).toString();
+                break;
+        }
+
+        setConvertedTs(currentText);
+    };
+
+    return (
+        <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+            <TextFieldWithCopy value={ts} setValue={setTs} />
+            <TextFieldWithCopy value={convertedTs} setValue={setConvertedTs} />
+            <Button onClick={handleChangeTimestamp}>Get Timestamp</Button>
+        </Flex>
+    );
+};
+
+const ChangeTextCaseComponent = () => {
+    const [text, setText] = useState('');
+    const [finalText, setFinalText] = useState('');
+    const [textCase, setCase] = useState(TEXT_CASES[0].value);
+
+    const handleChangeCase = () => {
+        var currentText = '';
+
+        switch (textCase) {
+            case TEXT_CASES[0].value:
+                currentText = text.toLowerCase();
+                break;
+            case TEXT_CASES[1].value:
+                currentText = text.toUpperCase();
+                break;
+            case TEXT_CASES[2].value:
+                currentText = changeCase.camelCase(text);
+                break;
+            case TEXT_CASES[3].value:
+                currentText = changeCase.capitalCase(text);
+                break;
+            case TEXT_CASES[4].value:
+                currentText = changeCase.constantCase(text);
+                break;
+            case TEXT_CASES[5].value:
+                currentText = changeCase.dotCase(text);
+                break;
+            case TEXT_CASES[6].value:
+                currentText = changeCase.headerCase(text);
+                break;
+            case TEXT_CASES[7].value:
+                currentText = changeCase.noCase(text);
+                break;
+            case TEXT_CASES[8].value:
+                currentText = changeCase.paramCase(text);
+                break;
+            case TEXT_CASES[9].value:
+                currentText = changeCase.pascalCase(text);
+                break;
+            case TEXT_CASES[10].value:
+                currentText = changeCase.pathCase(text);
+                break;
+            case TEXT_CASES[11].value:
+                currentText = changeCase.sentenceCase(text);
+                break;
+            case TEXT_CASES[12].value:
+                currentText = changeCase.snakeCase(text);
+                break;
+            default:
+                currentText = text.toLowerCase();
+                break;
+        }
+
+        setFinalText(currentText);
+    };
+
+    return (
+        <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+            <TextFieldWithCopy value={text} setValue={setText} />
+
+            <TextFieldWithCopy value={finalText} setValue={setFinalText} />
+            <Button onClick={handleChangeCase}>Convert</Button>
+        </Flex>
+    );
+};
+
+const Utility = () => {
+    return (
+        <SimpleGrid cols={2} spacing="sm" verticalSpacing="sm" py={10}>
+            <GetPublicIpComponent />
+            <GetTimestampComponent />
+            <ConvertTimestampComponent />
+            <ChangeTextCaseComponent />
         </SimpleGrid>
     );
 };
