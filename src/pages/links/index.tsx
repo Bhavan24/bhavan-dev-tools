6aafc21afe963bf98cbc0ba0130d6324220e2cf5
bhavan <bhavanl@techprovint.com>
2023-07-29 00:52:03 +0530

    links

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -1,5 +1,34 @@
+import { Select } from '@mantine/core';
+import { useEffect, useState } from 'react';
+
+enum Environments {
+    DEV = 'dev',
+    QA = 'qa',
+    PRE_PROD = 'pre-prod',
+    PROD = 'prod',
+}
+
+const ENVIRONMENTS = [Environments.DEV, Environments.QA, Environments.PRE_PROD, Environments.PROD];
+
 const Links = () => {
-    return <>Links</>;
+    const [searchValue, onSearchChange] = useState('');
+
+    useEffect(() => {
+        console.log(searchValue);
+    }, [searchValue]);
+
+    return (
+        <div style={{ margin: '10px' }}>
+            <Select
+                label="Select Environment"
+                onSearchChange={onSearchChange}
+                searchValue={searchValue}
+                nothingFound="No options"
+                placeholder="Pick one"
+                data={ENVIRONMENTS}
+            />
+        </div>
+    );
 };
 
 export { Links };
